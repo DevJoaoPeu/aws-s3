5010863957f4d0f0b0de5c440c1837545b5dc365
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-28 11:56:37 -0300

    feat: Finalizando configs para upload

--- src/ultils/S3Storage.ts
+++ src/ultils/S3Storage.ts
@@ -1,4 +1,8 @@
 import aws, { S3 } from "aws-sdk";
+import multerConfig from "../config/upload";
+import path from "path";
+import mime from "mime";
+import fs from "fs";
 
 class S3Storage {
   private client: S3;
@@ -7,6 +11,30 @@ class S3Storage {
     this.client = new aws.S3({
       region: "us-east-1",
     });
+  }
+
+  async saveFile(fileName: string): Promise<void> {
+    const originalPath = path.resolve(multerConfig.directory, fileName);
+
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      throw new Error("File not found");
+    }
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    this.client
+      .putObject({
+        Bucket: "teste-s3",
+        Key: fileName,
+        ACL: "public-read",
+        Body: fileContent,
+        ContentType,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalPath);
   }
 }
 
