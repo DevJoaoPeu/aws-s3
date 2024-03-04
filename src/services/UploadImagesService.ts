509607113c40aa5f8d0faa75475911c8d7574581
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-03-04 07:42:28 -0300

    feat: mandando arquivo para s3

--- src/services/UploadImagesService.ts
+++ src/services/UploadImagesService.ts
@@ -1,5 +1,9 @@
+import S3Storage from "../ultils/S3Storage";
+
 class UploadImagesService {
-  async execute(): Promise<void> {}
+  async execute(file: Express.Multer.File): Promise<void> {
+     await S3Storage.saveFile(file.filename)
+  }
 }
 
 export default new UploadImagesService();
